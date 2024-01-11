ebe294d024de7c84ca64587cc1ec389c3b740f1d
SIMEUNZI <145742347+SIMEUNZI@users.noreply.github.com>
2024-01-11 09:51:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Login from './pages/user/Login';
 import Signup from './pages/user/Signup';
 import Sell from './pages/user/Sell';
 
-import Payment from './pages/Payment';
 import Shop from './pages/product/Shop';
 import ProductList from './pages/product/ProductList';
 import ProductList2 from './pages/product/ProductList2';
@@ -36,7 +35,6 @@ function App () {
         <Route path="/procut-detail/:id" element={<ProductDetail />} />
         <Route path="/product-update/" element={<ProductUpdate />} />
         <Route path="/sell" element={<Sell />} />
-        <Route path="/payment" element={<Payment />} />
 
       </Route>
 
